6aa6724468dda1eeda218aca690eeba0d3325a9b
eko8757 <ekomurdiansyah89@gmail.com>
2020-08-20 00:52:01 +0700

    refactor app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,6 @@
 import React from 'react';
-import {
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import Router from './router';
-// import Welcome from './pages/Welcome';
 
 const App = () => {
   return (
@@ -17,13 +9,5 @@ const App = () => {
     </NavigationContainer>
   );
 };
-
-const styles = StyleSheet.create({
-  txtTitle: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
-});
 
 export default App;
