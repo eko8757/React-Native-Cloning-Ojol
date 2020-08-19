0513272178e0e912dd73acb39ba9654c95676750
eko8757 <ekomurdiansyah89@gmail.com>
2020-08-20 00:49:30 +0700

    change image welcome svg to image welcome png

---
+++ src/assets/index.js
@@ -0,0 +1,3 @@
+import ilustrationsWelcome from './image/ilustrations_welcome.png';
+
+export {ilustrationsWelcome};
--- src/pages/Welcome/index.js
+++ src/pages/Welcome/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { Text, View, TouchableOpacity } from 'react-native';
-import Ilustrations from '../../assets/image/ilustrations_welcome.svg';
+import { Text, View, TouchableOpacity, Image } from 'react-native';
 import ActionButton from './ActionButton';
 import { colors } from '../../utils';
+import { ilustrationsWelcome } from '../../assets';
 
 const Welcome = () => {
     return (
         <View style={styles.wrapper.page}>
-            <View style={styles.ilustrations}>
-                <Ilustrations width={320} height={250} />
+            <View style={styles.wrapper.ilustrations}>
+                <Image source={ilustrationsWelcome} style={styles.image} />
             </View>
             <Text style={styles.text.txtTitle}>Selamat Datang!</Text>
             <Text style={styles.text.txtContent}>Silahkan nikmati layanan kami, namun sebelum itu dimohon untuk Login terlebih dahulu jika sudah memiliki akun.</Text>
@@ -24,6 +24,9 @@ const styles = {
             flex: 1,
             padding: 20,
             backgroundColor: 'white',
+        },
+        ilustrations: {
+            alignItems: 'center',
         },
     },
     text: {
@@ -41,8 +44,9 @@ const styles = {
             color: colors.colorText.colorTextDark,
         },
     },
-    ilustrations: {
-        alignItems: 'center',
+    image: {
+        width: 330,
+        height: 260,
     },
 };
 
