cbeefe134e095bb4e856334c1333b22ffb10869d
eko8757 <ekomurdiansyah89@gmail.com>
2020-08-19 15:23:25 +0700

    create welcome screen

--- src/pages/Welcome/index.js
+++ src/pages/Welcome/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import Ilustrations from '../../assets/image/ilustrations_welcome.svg';
 
 const Welcome = () => {
     return (
         <View style={styles.container}>
-            <Text style={styles.txtTitle}>Welcome Screen</Text>
+            <View style={styles.wrapperImg}>
+                <Ilustrations width={320} height={250} />
+            </View>
+            <Text style={styles.txtTitle}>Selamat Datang!</Text>
+            <Text style={styles.txtContent}>Silahkan nikmati layanan kami, namun sebelum itu dimohon untuk Login terlebih dahulu jika sudah memiliki akun.</Text>
+            <TouchableOpacity>
+                <View style={styles.bgButton}>
+                    <Text style={styles.txtButton}>Login</Text>
+                </View>
+            </TouchableOpacity>
         </View>
     );
 };
@@ -16,7 +26,29 @@ const styles = StyleSheet.create({
     txtTitle: {
         textAlign: 'center',
         fontWeight: 'bold',
+        fontSize: 18,
+        marginTop: 8,
     },
+    txtContent: {
+        textAlign: 'center',
+        fontSize: 14,
+        marginTop: 8,
+    },
+    wrapperImg: {
+        alignItems: 'center',
+    },
+    bgButton: {
+        backgroundColor: '#6C63FF',
+        borderRadius: 8,
+        paddingVertical: 10,
+        marginTop: 24,
+    },
+    txtButton: {
+        fontSize: 18,
+        fontWeight: '600',
+        color: '#FFFFFF',
+        textAlign: 'center',
+    }
 });
 
 export default Welcome;
