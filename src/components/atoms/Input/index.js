3caa3fa4746d6948f69651fdb492f7bf5ef7c9df
eko8757 <ekomurdiansyah89@gmail.com>
2020-08-20 20:12:33 +0700

    add text input login

---
+++ src/components/atoms/Input/index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { StyleSheet, Text, TextInput } from 'react-native'
+import { colors } from '../../../utils';
+
+const Input = ({placeholder}) => {
+    return (
+        <TextInput
+            style={styles.input}
+            placeholder={placeholder}
+            placeholderTextColor={colors.colorPrimary}
+        />
+    );
+};
+
+const styles = StyleSheet.create({
+    input: {
+        borderWidth: 1,
+        borderColor: colors.colorPrimary,
+        borderRadius: 8,
+        paddingVertical: 12,
+        paddingHorizontal: 16,
+        fontSize: 14,
+        color: colors.colorText.colorTextDark,
+    },
+});
+
+export default Input;
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,14 +1,25 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { Text, View } from 'react-native'
+import { Input } from '../../components';
 
 const Login = () => {
     return (
-        <View>
+        <View style={styles.wrapper.page}>
             <Text>Login Page</Text>
+            <Input placeholder="Email.." />
+            <Input placeholder="Password.." />
         </View>
     );
 };
 
-const styles = StyleSheet.create({})
+const styles = {
+    wrapper: {
+        page: {
+            flex: 1,
+            padding: 20,
+            backgroundColor: 'white',
+        },
+    },
+};
 
 export default Login;
