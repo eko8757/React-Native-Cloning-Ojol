90311e6fd34e04be9796a7c6c049226b6e542b13
eko8757 <ekomurdiansyah89@gmail.com>
2020-08-31 14:33:58 +0700

    add state management use redux in login page

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Text, View, ScrollView } from 'react-native'
 import { Input, Button } from '../../components';
 import { setForm } from '../../redux';
--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Text, View, ScrollView } from 'react-native';
 import { Input, Button } from '../../components';
 import { useSelector, useDispatch } from 'react-redux';
